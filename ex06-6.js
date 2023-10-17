e0d36b2a12bba52fdb695a169d901e805eeb0b2e
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2023-10-17 10:25:33 +0900

    Update ex06-6.js

--- ex06-6.js
+++ ex06-6.js
@@ -1,12 +1,12 @@
-var who66 = 'world';
-function sayhello66() {
-alert('Hello, ' + who66 + '!');
-}
-function taro66() {
-alert('太郎が来た')
-who66 = '太郎';
-}
-function hanako66() {
-alert('花子が来た')
-who66 = '花子';
-}
+var who65 ='world';
+
+function sayhello66(){ alert('Hello,'+ who66 +'!');}
+
+var who1 ='someone';
+
+function coming1(){ alert(who1 + 'が来た!');}
+
+
+function taro66(){ who66 = '太郎'; who1 = '太郎';}
+
+function hanako66(){ who66 ='花子'; who1 ='花子';}
