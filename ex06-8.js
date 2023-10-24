c4e65e12d915ffd3ad34b9311a674e757d8c95bb
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2023-10-24 10:26:50 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -7,3 +7,9 @@ document.getElementById('who68').innerHTML = '太郎';
 function hanako68() {
 document.getElementById('who68').innerHTML = '花子';
 }
+
+
+function gazo688()
+{document.getElementById('who688').innerHTML=
+'<img src="./image/bulbon.gif" alt="オン">’
+;}
