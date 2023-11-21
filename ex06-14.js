713959f1a64da3b9f10a1fc231ebc012d93d143a
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2023-11-21 10:06:39 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,14 @@
 function mes() { 
 alert("3秒経ちました！"); 
 }
+
+
+function imgchange5() {
+document.getElementById('logo').src = 
+'./image/inu2.png';
+}
+
+function imgchange6() {
+document.getElementById('logo').src = 
+'./image/inu1.png';
+}
